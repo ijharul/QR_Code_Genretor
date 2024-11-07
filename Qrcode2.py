4dea1445c92df5c6e06c0abcf775b86c93a239a4
Ijharul <haqueijharul078@gmail.com>
2024-11-07 23:58:57 +0530

    update

---
+++ Qrcode2.py
@@ -0,0 +1,9 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4,)
+qr.add_data("https://www.linkedin.com/in/ijharul-haque-896394300/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="blue")
+img.save("Ijharul_Linkedin.png")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QR_Code_Genretor
 python mini project for generate Qr_code
+ijharul_Git_hub code in main.py file
+ijharul_Linkedin code in Qrcode2.py file
